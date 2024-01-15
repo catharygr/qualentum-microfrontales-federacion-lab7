4c0c4f032dc4af4b674de56f8daddb998d08e702
catharygr <103951447+catharygr@users.noreply.github.com>
2024-01-15 14:49:42 +0000

    Instalado host

---
+++ packages/host/src/App.js
@@ -0,0 +1,10 @@
+import "./index.css";
+
+document.getElementById("app").innerHTML = `
+<div class="container">
+  <div>Name: host</div>
+  <div>Framework: vanilla</div>
+  <div>Language: JavaScript</div>
+  <div>CSS: Empty CSS</div>
+</div>
+`;
