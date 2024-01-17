c1ea2fb8a067de21819f29f7d96838a81fb3508c
catharygr <103951447+catharygr@users.noreply.github.com>
2024-01-17 13:31:52 +0000

    Trabajando en el web-componet header-component

--- packages/header/src/Header.js
+++ packages/header/src/Header.js
@@ -3,14 +3,18 @@ import { Menu } from "../../web-components/menu.js";
 import * as moment from "moment";
 
 export const header = () => {
-  const app = document.getElementById("app");
-  const header = document.createElement("header");
-  header.classList.add("header");
-
-  // Atributos de menu-global: vertical que seria un (boleano) y color que seria un string
-  header.innerHTML = `
-<menu-global color="yellow"></menu-global>
-<p>${moment().format("DD/MM/YYYY")}</p>
-    `;
-  app.appendChild(header);
+  //   const app = document.getElementById("app");
+  //   const header = document.createElement("header");
+  //   header.classList.add("header");
+  //   // Atributos de menu-global: vertical que seria un (boleano) y color que seria un string
+  //   header.innerHTML = `
+  // <menu-global color="yellow"></menu-global>
+  // <p>${moment().format("DD/MM/YYYY")}</p>
+  //     `;
+  //   app.appendChild(header);
+  // return `
+  //   <header>
+  //     <p>popo</p>
+  //   </header>
+  // `;
 };
--- packages/header/src/header-component.js
+++ packages/header/src/header-component.js
@@ -1,4 +1,6 @@
 import { LitElement, html, css } from "lit";
+import * as moment from "moment";
+import { Menu } from "../../web-components/menu.js";
 
 export class HeaderComponent extends LitElement {
   constructor() {
