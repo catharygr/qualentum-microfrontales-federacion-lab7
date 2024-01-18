7d017d08267695c4d7147461226c2f43e70f6ae3
catharygr <103951447+catharygr@users.noreply.github.com>
2024-01-18 11:19:57 +0000

    Trabajando en header-component

--- packages/header/src/Header.js
+++ packages/header/src/Header.js
@@ -1,19 +1,20 @@
 import "./index.css";
+import { Menu } from "../../web-components/menu.js";
 import * as moment from "moment";
 
 export const header = () => {
-  //   const app = document.getElementById("app");
-  //   const header = document.createElement("header");
-  //   header.classList.add("header");
-  //   // Atributos de menu-global: vertical que seria un (boleano) y color que seria un string
-  //   header.innerHTML = `
-  // <menu-global color="yellow"></menu-global>
-  // <p>${moment().format("DD/MM/YYYY")}</p>
-  //     `;
-  //   app.appendChild(header);
-  // return `
-  //   <header>
-  //     <p>popo</p>
-  //   </header>
-  // `;
+  const app = document.getElementById("app");
+  const header = document.createElement("header");
+  header.classList.add("header");
+  // Atributos de menu-global: vertical que seria un (boleano) y color que seria un string
+  header.innerHTML = `
+  <menu-global color="yellow"></menu-global>
+  <p>${moment().format("DD/MM/YYYY")}</p>
+      `;
+  app.appendChild(header);
+  return `
+    <header>
+      <p>popo</p>
+    </header>
+  `;
 };
