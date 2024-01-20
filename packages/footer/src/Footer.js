17ea2f318cf0f5c4a2dcbda39038bdb80fa231ec
catharygr <103951447+catharygr@users.noreply.github.com>
2024-01-20 15:13:45 +0000

    first commit

--- packages/footer/src/Footer.js
+++ packages/footer/src/Footer.js
@@ -1,8 +1,32 @@
-import "./index.css";
-import "./footer-component.js";
+import { html, css, LitElement } from "lit";
+import * as moment from "moment";
+import "../../web-components/menu.js";
 
-export const footer = () => {
-  const app = document.getElementById("app");
-  const footerComponent = document.createElement("footer-component");
-  app.appendChild(footerComponent);
-};
+export class FooterComponent extends LitElement {
+  constructor() {
+    super();
+  }
+
+  static styles = css`
+    footer {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      background-color: lightslategray;
+      color: black;
+      padding: 1rem 2rem;
+      font-weight: 900;
+      gap: 1.5rem;
+    }
+  `;
+
+  render() {
+    return html`
+      <footer>
+        <menu-global vertical color="orange"></menu-global>
+        <p>${moment().format("DD/MM/YYYY")}</p>
+      </footer>
+    `;
+  }
+}
